e33fdc24e5ac637b66a71b6630ccdad42dfc2864
Evan Wheeler <anon0mys87@gmail.com>
2018-07-12 08:59:09 -0600

    Go dependencies for Heroku

--- internal/models/meal.go
+++ internal/models/meal.go
@@ -26,3 +26,30 @@ func (m *Meal) GetMeal(db *gorm.DB) Meal {
 
   return meal
 }
+
+func (m *Meal) AddMealFood(db *gorm.DB, foodId string) Output {
+  var meal Meal
+  var food Food
+
+  if err := db.First(&meal, m.ID).Error; err != nil {
+    return err
+  }
+
+  if err := db.First(&food, foodID).Error; err != nil {
+    return err
+  }
+
+  meal_food := MealFood{Food_id: food.ID, Meal_id: meal.ID}
+  db.NewRecord(meal_food)
+  if err := db.Create(&meal_food).Error; err != nil {
+    return err
+  }
+
+  output := struct {
+    Message string
+   }{
+     Message: fmt.Sprintf("Successfully added %s to %s", food.Name, meal.Name)
+     }
+     
+  return output
+}
